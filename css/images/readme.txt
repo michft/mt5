f4e52c7d25f1f40b639289d670480d7bb821d594
root <root@mint>
2012-10-18 09:23:00 +1100

    Inital copy to github

---
+++ css/images/readme.txt
@@ -0,0 +1,3 @@
+Stupid cludge to cover some SUMS site JEvents until I get access to that code!
+
+Michael 20110224
---
+++ js/layout.js
@@ -0,0 +1,71 @@
+// - - - - - - - - - - - - - - - - - - - - -
+//
+// Title : Dynamic Resolution Dependent Layout Demo
+// Author : Kevin Hale
+// URL : http://particletree.com
+//
+// Description : This is a demonstration of a dynamic 
+// resolution dependent layout in action. Change your browser 
+// window size to see the layout respond to your changes. To 
+// preserve the separation of the presentation and behavior 
+// layers, this implementation delegates all the presentation 
+// details to external CSS stylesheets instead of changing 
+// each style property through JavaScript.
+//
+// Created : July 30, 2005
+// Modified : November 15, 2005
+// Modified : 2011-01-08 Michael Tomkins http://www.mich431.net
+//
+// - - - - - - - - - - - - - - - - - - - - -
+
+// getBrowserWidth is taken from The Man in Blue Resolution Dependent Layout Script
+// http://www.themaninblue.com/experiment/ResolutionLayout/
+	function getBrowserWidth(){
+		if (window.innerWidth){
+			return window.innerWidth;}	
+		else if (document.documentElement && document.documentElement.clientWidth != 0){
+			return document.documentElement.clientWidth;	}
+		else if (document.body){return document.body.clientWidth;}		
+			return 0;
+	}
+
+// dynamicLayout by Kevin Hale
+function dynamicLayout(){
+	var browserWidth = getBrowserWidth();
+
+
+	//Load mobile.css Rules, 1 Col, navs in top, no menu
+	if (browserWidth < 650){
+		changeLayout("mobile");
+	}
+	//Load default.css Rules, 3 col, menu
+	if (browserWidth >= 650){
+		changeLayout("default");
+	}
+}
+
+// changeLayout is based on setActiveStyleSheet function by Paul Sowdon 
+// http://www.alistapart.com/articles/alternate/
+function changeLayout(description){
+   var i, a;
+   for(i=0; (a = document.getElementsByTagName("link")[i]); i++){
+	   if(a.getAttribute("title") == description){a.disabled = false;}
+	   else if(a.getAttribute("title") != "default"){a.disabled = true;}
+   }
+}
+
+	//addEvent() by John Resig
+	function addEvent( obj, type, fn ){ 
+	   if (obj.addEventListener){ 
+	      obj.addEventListener( type, fn, false );
+	   }
+	   else if (obj.attachEvent){ 
+	      obj["e"+type+fn] = fn; 
+	      obj[type+fn] = function(){ obj["e"+type+fn]( window.event ); } 
+	      obj.attachEvent( "on"+type, obj[type+fn] ); 
+	   } 
+	} 
+	
+	//Run dynamicLayout function when page loads and when it resizes.
+	addEvent(window, 'load', dynamicLayout);
+	addEvent(window, 'resize', dynamicLayout);
